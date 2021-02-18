5c661c06b5a18c87e15332f8040816e51de3fd85
Dmitrii Petorai <dmpetlaj@gmail.com>
2021-02-19 00:17:55 +0300

    add styles

--- src/components/LeftNavigation.vue
+++ src/components/LeftNavigation.vue
@@ -1,33 +1,32 @@
 <template>
-  <b-col md="3" class="todo-left-navigation">
-      <ul class="list-group">
-        <li>
-          <b-icon-bookmark />
-          Входящие
-        </li>
-        <li>
-          <b-icon-bookmark-plus />
-          Входящие для команды
-        </li>
-        <li>
-          <b-icon-calendar />
-          Сегодня
-        </li>
-        <li>
-          <b-icon-calendar4-event />
-          Предстоящие
-        </li>
-        <li>
-          <b-icon-exclamation-diamond />
-          Дела
-        </li>
-      </ul>
+  <b-col md="4" class="todo-left-navigation">
+    <ul class="list-group">
+      <li>
+        <b-icon-bookmark />
+        <span class="list-group__item">Входящие</span>
+      </li>
+      <li>
+        <b-icon-bookmark-plus />
+        <span class="list-group__item">Входящие для команды</span>
+      </li>
+      <li>
+        <b-icon-calendar />
+        <span class="list-group__item">Сегодня</span>
+      </li>
+      <li>
+        <b-icon-calendar4-event />
+        <span class="list-group__item">Предстоящие</span>
+      </li>
+      <li>
+        <b-icon-exclamation-diamond />
+        <span class="list-group__item">Дела</span>
+      </li>
+    </ul>
   </b-col>
 </template>
 
 <script>
 export default {
   name: 'LeftNavigation'
-
 }
 </script>
