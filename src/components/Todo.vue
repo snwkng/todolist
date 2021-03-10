b53600ec57cf9c7468a3f557dd475de7a26137a6
Dmitrii Petorai <dmpetlaj@gmail.com>
2021-03-11 01:28:57 +0300

    change todo component

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -3,8 +3,18 @@
    <div class="todo-header">
      <h1>{{activeGroup.group_name}}</h1>
    </div>
-   <div class="todo-list">
-   </div>
+   <ul class="todo-list">
+     <li class="todo-list__item">
+       <input type="checkbox" class="item-checkbox" />
+       <div class="item-content">
+         <div class="item-content__main">
+            <div class="item-content__main-name">Test todo</div>
+         <div class="item-content__main-option"></div>
+         </div>
+         <div class="item-content__other">18 ferbuary</div>
+       </div>
+     </li>
+   </ul>
  </div>
 </template>
 
