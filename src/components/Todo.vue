27fbe140bbe8bcd3afa87b4ff02c9ef4cb5f8907
Dmitrii Petorai <dmpetlaj@gmail.com>
2021-03-29 23:33:02 +0300

    update todo logic add

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -4,28 +4,47 @@
      <h1>{{activeGroup.group_name}}</h1>
    </div>
    <ul class="todo-list">
-     <li class="todo-list__item" v-for="todo in todosByGroup" :key="todo._id">
-       <input type="checkbox" class="item-checkbox" v-model="todo.todo_done" @change="doneTodo(todo)" />
-       <div class="item-content">
-         <div class="item-content__main">
-            <div class="item-content__main-name">{{ todo.todo_name }}</div>
-         <div class="item-content__main-option"></div>
-         </div>
-         <div class="item-content__other">
-           <v-icon class="item-content__other-icon" name="calendar"></v-icon>
-           <span class="item-content__other-text">18 марта</span>
-         </div>
+     <li v-for="todo in todosByGroup" :key="todo._id">
+       <div class="" v-if="editableTodo && editableTodo._id === todo._id">
+        <div class="todo-editor">
+          <textarea
+            autocorrect="on"
+            autofocus
+            type="text"
+            class="editor todo-editor__area"
+            ref="editTodo"
+            v-model="editableTodo.todo_name"
+            @keydown.esc="cancelCreate">
+          </textarea>
+        </div>
+        <div class="create-list__options">
+          <button type="button" class="create" @click="doneTodo(editableTodo)">Сохранить</button>
+          <button type="button" class="cancel" @click="cancelCreate">Отмена</button>
+        </div>
        </div>
-       <div class="item-options">
-         <div class="item-options__option" title="edit">
-           <v-icon class="icon" name="edit-3"></v-icon>
-         </div>
-         <div class="item-options__option" title="delete" @click="deleteTodo(todo)">
-           <v-icon class="icon" name="trash-2"></v-icon>
-         </div>
-         <div class="item-options__option" title="other settings">
-           <v-icon class="icon" name="more-horizontal"></v-icon>
-         </div>
+       <div class="todo-list__item" v-else>
+        <input type="checkbox" class="item-checkbox" v-model="todo.todo_done" @change="doneTodo(todo)" />
+        <div class="item-content">
+          <div class="item-content__main">
+              <div class="item-content__main-name">{{ todo.todo_name }}</div>
+          <div class="item-content__main-option"></div>
+          </div>
+          <div class="item-content__other">
+            <v-icon class="item-content__other-icon" name="calendar"></v-icon>
+            <span class="item-content__other-text">18 марта</span>
+          </div>
+        </div>
+        <div class="item-options">
+          <div class="item-options__option" title="edit" @click="editTodo(todo)">
+            <v-icon class="icon" name="edit-3"></v-icon>
+          </div>
+          <div class="item-options__option" title="delete" @click="deleteTodo(todo)">
+            <v-icon class="icon" name="trash-2"></v-icon>
+          </div>
+          <div class="item-options__option" title="other settings">
+            <v-icon class="icon" name="more-horizontal"></v-icon>
+          </div>
+        </div>
        </div>
      </li>
    </ul>
@@ -69,6 +88,7 @@ export default {
   name: 'Todo',
   data () {
     return {
+      editableTodo: null,
       showCreateTodoEditor: false,
       todo: '',
       done: false
@@ -80,12 +100,18 @@ export default {
   },
   methods: {
     ...mapActions('todo', ['GET_TODOS', 'GET_TODOS_BY_GROUP']),
+    focusInput () {
+      if (this.$refs.editTodo && this.$refs.editTodo.length > 0) {
+        this.$refs.editTodo[0].focus()
+      }
+    },
     createTodo () {
       this.showCreateTodoEditor = true
     },
     cancelCreate () {
       this.showCreateTodoEditor = false
       this.todo = ''
+      this.editableTodo = null
     },
     addTodo () {
       const todo = {
@@ -101,11 +127,21 @@ export default {
       store.dispatch('todo/DELETE_TODO', todo)
     },
     doneTodo (todo) {
-      store.dispatch('todo/UPDATE_TODO', todo)
+      store.dispatch('todo/UPDATE_TODO', todo).then(() => {
+        if (typeof todo === 'object') {
+          this.editableTodo = null
+        }
+      })
+    },
+    editTodo (todo) {
+      this.editableTodo = Object.assign({}, todo)
     }
   },
   created () {
     this.GET_TODOS()
+  },
+  mounted () {
+    this.focusInput()
   }
 }
 </script>
